159705c94937358807ff6efd66c2de1c7fffa776
zhushuai <zhushuai@sunlands.com>
2018-12-03 09:07:09 +0800

    calling api

--- src/apis/home/index.js
+++ src/apis/home/index.js
@@ -2,7 +2,7 @@ import fetch from '../../libs/fetch';
 
 // login by usn and pwd
 const login = data => {
-  const url = '';
+  const url = '/auth/login';
 
   return fetch({url, method: 'post', data});
 };
